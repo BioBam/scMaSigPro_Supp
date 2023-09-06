c7497ad22af0dfc0faa0fb449838563ac1e11d26
spriyansh <spriyansh29@gmail.com>
2023-09-06 17:22:48 +0200

    Added Implementation of Real Dataset

--- benchmarks/11_RealDataSmall/script/02_Cell_Type_Annotation.R
+++ benchmarks/11_RealDataSmall/script/02_Cell_Type_Annotation.R
@@ -30,21 +30,21 @@ sob <- LoadH5Seurat(file = paste0(prefixIn, "rep1_Processed_sob.h5seurat"), verb
 
 # Azimuth Annotations
 bm <- RunAzimuth(
-    query = sob,
-    reference = paste0(
-        str_sub(prefixIn, end = -9),
-        "input/Azimuth_Human_BoneMarrow"
-    )
+  query = sob,
+  reference = paste0(
+    str_sub(prefixIn, end = -9),
+    "input/Azimuth_Human_BoneMarrow"
+  )
 )
 
 # Plot the Annotations
 p <- DimPlot(bm, reduction = "umap", pt.size = 1, group.by = "predicted.celltype.l2") +
-    ggtitle(paste0("Individual: ", individual, " Age: ", age, " Sex: ", sex)) +
-    scale_color_hue(l = 50) + theme(legend.position = "bottom")
+  ggtitle(paste0("Individual: ", individual, " Age: ", age, " Sex: ", sex)) +
+  scale_color_hue(l = 50) + theme(legend.position = "bottom")
 
 ggsave(p,
-       filename = paste0(prefixOut, i, "_All_Anno_Azimuth.png"),
-       dpi = 1400, limitsize = FALSE, width = 8, height = 8
+  filename = paste0(prefixOut, i, "_All_Anno_Azimuth.png"),
+  dpi = 1400, limitsize = FALSE, width = 8, height = 8
 )
 
 # Subset the Scores
@@ -52,48 +52,48 @@ bm.score <- subset(bm, subset = predicted.celltype.l2.score >= 0.6)
 
 # Plot the Annotations
 p <- DimPlot(bm.score, reduction = "umap", pt.size = 1, group.by = "predicted.celltype.l2") +
-    ggtitle(paste0("Individual: ", individual, " Age: ", age, " Sex: ", sex)) +
-    scale_color_hue(l = 50) + theme(legend.position = "bottom")
+  ggtitle(paste0("Individual: ", individual, " Age: ", age, " Sex: ", sex)) +
+  scale_color_hue(l = 50) + theme(legend.position = "bottom")
 
 # Save
 ggsave(p,
-       filename = paste0(prefixOut, i, "_Score_Sub_Anno_Azimuth.png"),
-       dpi = 1400, limitsize = FALSE, width = 8, height = 8
+  filename = paste0(prefixOut, i, "_Score_Sub_Anno_Azimuth.png"),
+  dpi = 1400, limitsize = FALSE, width = 8, height = 8
 )
 
 # Subset the Cell types
 bm.cell <- subset(bm.score,
-                  subset = predicted.celltype.l2 %in% c(
-                      "Early Eryth",
-                      "GMP", "HSC",
-                      "LMPP", "Late Eryth",
-                      "CLP", "EMP"
-                  )
+  subset = predicted.celltype.l2 %in% c(
+    "Early Eryth",
+    "GMP", "HSC",
+    "LMPP", "Late Eryth",
+    "CLP", "EMP"
+  )
 )
 
 # Plot the Annotations
 p <- DimPlot(bm.cell, reduction = "umap", pt.size = 1, group.by = "predicted.celltype.l2") +
-    ggtitle(paste0("Individual: ", individual, " Age: ", age, " Sex: ", sex)) +
-    scale_color_hue(l = 50) + theme(legend.position = "bottom")
+  ggtitle(paste0("Individual: ", individual, " Age: ", age, " Sex: ", sex)) +
+  scale_color_hue(l = 50) + theme(legend.position = "bottom")
 
 ggsave(p,
-       filename = paste0(prefixOut, i, "_Cell_Sub_Anno_Azimuth.png"),
-       dpi = 1400, limitsize = FALSE, width = 8, height = 8
+  filename = paste0(prefixOut, i, "_Cell_Sub_Anno_Azimuth.png"),
+  dpi = 1400, limitsize = FALSE, width = 8, height = 8
 )
 
 # Plot Clusters
 p <- DimPlot(bm.cell, reduction = "umap", pt.size = 1, group.by = "seurat_clusters") +
-    ggtitle(paste0("Individual: ", individual, " Age: ", age, " Sex: ", sex)) +
-    scale_color_hue(l = 50) + theme(legend.position = "bottom")
+  ggtitle(paste0("Individual: ", individual, " Age: ", age, " Sex: ", sex)) +
+  scale_color_hue(l = 50) + theme(legend.position = "bottom")
 
 ggsave(p,
-       filename = paste0(prefixOut, i, "_Seurat_Clusters.png"),
-       dpi = 1400, limitsize = FALSE, width = 8, height = 8
+  filename = paste0(prefixOut, i, "_Seurat_Clusters.png"),
+  dpi = 1400, limitsize = FALSE, width = 8, height = 8
 )
 
 # Write Seurat H5
 file_name <- paste0(prefixOut, i, "_Annotated_sob")
 SaveH5Seurat(
-    object = bm.cell, filename = file_name,
-    overwrite = T, verbose = FALSE
+  object = bm.cell, filename = file_name,
+  overwrite = T, verbose = FALSE
 )
--- benchmarks/11_RealDataSmall/script/03_Pseudotimme_Inference.R
+++ benchmarks/11_RealDataSmall/script/03_Pseudotimme_Inference.R
@@ -31,26 +31,28 @@ sob <- LoadH5Seurat(file = paste0(prefixIn, "rep1_Annotated_sob.h5seurat"), verb
 
 # Plot
 DimPlot(sob, reduction = "umap", pt.size = 1, group.by = "predicted.celltype.l2") +
-    ggtitle(paste0("Individual: ", individual, " Age: ", age, " Sex: ", sex)) +
-    scale_color_hue(l = 50) + theme(legend.position = "bottom")
+  ggtitle(paste0("Individual: ", individual, " Age: ", age, " Sex: ", sex)) +
+  scale_color_hue(l = 50) + theme(legend.position = "bottom")
 
 # Construct Cell Dataset object
 cds <- new_cell_data_set(
-    expression_data = sob@assays$RNA@counts,
-    cell_metadata = DataFrame(sob@meta.data),
-    gene_metadata = data.frame(
-        gene_short_name = rownames(sob@assays$RNA@counts),
-        row.names = rownames(sob@assays$RNA@counts)
-    )
+  expression_data = sob@assays$RNA@counts,
+  cell_metadata = DataFrame(sob@meta.data),
+  gene_metadata = data.frame(
+    gene_short_name = rownames(sob@assays$RNA@counts),
+    row.names = rownames(sob@assays$RNA@counts)
+  )
 )
 
 # Pre-process
 cds <- preprocess_cds(cds, num_dim = 5)
 
 # Reduce dimensions
-cds <- reduce_dimension(cds, max_components = 2,
-                        umap.min_dist = 0.5,
-                        preprocess_method = "PCA")
+cds <- reduce_dimension(cds,
+  max_components = 2,
+  umap.min_dist = 0.5,
+  preprocess_method = "PCA"
+)
 
 # Cluster the Data
 cds <- cluster_cells(cds)
@@ -62,8 +64,8 @@ cds <- learn_graph(cds)
 trajectory.graph <- plot_cells(cds, color_cells_by = "predicted.celltype.l2")
 
 ggsave(trajectory.graph,
-       filename = paste0(prefixOut, i, "_trajectory_graph.png"),
-       dpi = 1400, limitsize = FALSE, width = 8, height = 8
+  filename = paste0(prefixOut, i, "_trajectory_graph.png"),
+  dpi = 1400, limitsize = FALSE, width = 8, height = 8
 )
 
 
@@ -74,15 +76,17 @@ cell_metadata <- as.data.frame(colData(cds))
 root_barcodes <- rownames(cell_metadata[cell_metadata$predicted.celltype.l2 == "HSC", ])
 
 # Calculate the Pseudotime
-cds <- order_cells(cds, reduction_method = "UMAP",
-            root_cells = root_barcodes)
+cds <- order_cells(cds,
+  reduction_method = "UMAP",
+  root_cells = root_barcodes
+)
 
 # Plot the Graph
 pseudotime.umap <- plot_cells(cds, color_cells_by = "pseudotime")
 
 ggsave(pseudotime.umap,
-       filename = paste0(prefixOut, i, "_pseudotime_umap.png"),
-       dpi = 1400, limitsize = FALSE, width = 8, height = 8
+  filename = paste0(prefixOut, i, "_pseudotime_umap.png"),
+  dpi = 1400, limitsize = FALSE, width = 8, height = 8
 )
 
 # Save
@@ -93,48 +97,48 @@ save(cds, file = paste0(prefixOut, "monocle3_inferred_pseudotime.RData"))
 # HPO in Monocyte Like cells
 p <- plot_genes_in_pseudotime(cds[rowData(cds)$gene_short_name == "MPO", ], color_cells_by = "predicted.celltype.l2")
 ggsave(p,
-       filename = paste0(prefixOut, i, "_MPO_Trend.png"),
-       dpi = 600, limitsize = FALSE
+  filename = paste0(prefixOut, i, "_MPO_Trend.png"),
+  dpi = 600, limitsize = FALSE
 )
 
 # GATA1 in Dendritic like cells
 p <- plot_genes_in_pseudotime(cds[rowData(cds)$gene_short_name == "GATA1", ], color_cells_by = "predicted.celltype.l2")
 ggsave(p,
-       filename = paste0(prefixOut, i, "_GATA1_Trend.png"),
-       dpi = 600, limitsize = FALSE
+  filename = paste0(prefixOut, i, "_GATA1_Trend.png"),
+  dpi = 600, limitsize = FALSE
 )
 
 # GATA2
 p <- plot_genes_in_pseudotime(cds[rowData(cds)$gene_short_name == "GATA2", ], color_cells_by = "predicted.celltype.l2")
 ggsave(p,
-       filename = paste0(prefixOut, i, "_GATA2_Trend.png"),
-       dpi = 600, limitsize = FALSE
+  filename = paste0(prefixOut, i, "_GATA2_Trend.png"),
+  dpi = 600, limitsize = FALSE
 )
 
 # CD34 in stem cell like cells
 p <- plot_genes_in_pseudotime(cds[rowData(cds)$gene_short_name == "CD34", ], color_cells_by = "predicted.celltype.l2")
 ggsave(p,
-       filename = paste0(prefixOut, i, "_CD34_Trend.png"),
-       dpi = 600, limitsize = FALSE
+  filename = paste0(prefixOut, i, "_CD34_Trend.png"),
+  dpi = 600, limitsize = FALSE
 )
 
 #  IRF8
 p <- plot_genes_in_pseudotime(cds[rowData(cds)$gene_short_name == "IRF8", ], color_cells_by = "predicted.celltype.l2")
 ggsave(p,
-       filename = paste0(prefixOut, i, "_IRF8_Trend.png"),
-       dpi = 600, limitsize = FALSE
+  filename = paste0(prefixOut, i, "_IRF8_Trend.png"),
+  dpi = 600, limitsize = FALSE
 )
 
 # EPOR in erythocyte
 p <- plot_genes_in_pseudotime(cds[rowData(cds)$gene_short_name == "EPOR", ], color_cells_by = "predicted.celltype.l2")
 ggsave(p,
-       filename = paste0(prefixOut, i, "_EPOR_Trend.png"),
-       dpi = 600, limitsize = FALSE
+  filename = paste0(prefixOut, i, "_EPOR_Trend.png"),
+  dpi = 600, limitsize = FALSE
 )
 
 # Down in GMP
 p <- plot_genes_in_pseudotime(cds[rowData(cds)$gene_short_name == "APOBEC3C", ], color_cells_by = "predicted.celltype.l2")
 ggsave(p,
-       filename = paste0(prefixOut, i, "_APOBEC3C_Trend.png"),
-       dpi = 600, limitsize = FALSE
+  filename = paste0(prefixOut, i, "_APOBEC3C_Trend.png"),
+  dpi = 600, limitsize = FALSE
 )
