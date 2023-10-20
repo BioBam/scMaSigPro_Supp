b5440684614f6d1e7e8bd4d33ca00f3ca2ff1739
Priyansh Srivastava <spriyansh29@gmail.com>
2023-10-20 09:53:28 +0000

    Initiated Analysis of Public Data

--- Analysis_Public_Data/script/01_Raw_to_processed.R
+++ Analysis_Public_Data/script/01_Raw_to_processed.R
@@ -15,16 +15,102 @@ suppressPackageStartupMessages(library(Matrix))
 suppressPackageStartupMessages(library(Azimuth))
 suppressPackageStartupMessages(library(SeuratData))
 suppressPackageStartupMessages(library(SeuratDisk))
-
-num_cores <- 16
+suppressPackageStartupMessages(library(parallel))
 
 # Prefix
-prefixIn <- "benchmarks/11_RealDataSmall/data/input/"
-prefixOut <- "benchmarks/11_RealDataSmall/data/results/"
+prefixIn <- "Analysis_Public_Data/data"
+prefixOut <- "Analysis_Public_Data/data"
 
 # Read BioMart info
-biomart.anno <- readRDS(paste0(prefixIn, "cell_cycle_data.mart"))
+biomart.anno <- readRDS(paste(prefixIn, "cell_cycle_data.mart", sep = "/"))
 reg.out <- c(unique(biomart.anno$SYMBOL))
+
+# Get folder names
+rep_vec <- list.dirs(prefixIn, full.names = F, recursive = F)
+names(rep_vec) <- rep_vec
+
+# Run lapply
+measures <- lapply(rep_vec, function(rep_i, inPath = prefixIn, outPath = prefixOut){
+    
+    # Step-1: Add Annotation for donors
+    if (rep_i == "rep1") {
+        individual <- "Donor-1"
+        age <- "35"
+        sex <- "Male"
+    } else if (rep_i == "rep2") {
+        individual <- "Donor-2"
+        age <- "28"
+        sex <- "Female"
+    } else if (rep_i == "rep3") {
+        individual <- "Donor-3"
+        age <- "19"
+        sex <- "Female"
+    }
+    
+    # Step-2: Load the filtered Matrix
+    filt_mat <- Read10X_h5(
+        paste(inPath, rep_i, "filtered_feature_bc_matrix.h5", sep = "/")
+        )
+    # Renaming the features
+    filt_mat@Dimnames[[1]] <- str_replace(filt_mat@Dimnames[[1]], "_", "-")
+    
+    # Create Raw Seurat Object
+    sob.raw <- CreateSeuratObject(
+        counts = filt_mat, min.cells = 200,
+        min.features = 100, project = rep_i
+    )
+    
+    # Calculate Percentage of Mitochondrial Reads
+    sob.raw[["percent.mt"]] <- PercentageFeatureSet(sob.raw, pattern = "^MT-")
+    
+    # Remove Cells 
+    sob.sub <- subset(sob.raw, subset = nFeature_RNA > 200 & nCount_RNA < 30000 & percent.mt < 7)
+    
+    # Normalize
+    sob.prs <- NormalizeData(sob.sub, verbose = F)
+    
+    # Find Variable features
+    #sob.prs <- FindVariableFeatures(sob.prs, selection.method = "vst", verbose = F, )
+    
+    # Check how many genes are present in the dataset
+    indata <- rownames(sob.prs)[rownames(sob.prs) %in% reg.out]
+    
+    # Keep only those which are present in data
+    biomart.anno <- biomart.anno[biomart.anno$SYMBOL %in% indata, ]
+    
+    # For seurat we need to divide genes into vectors
+    s.genes <- unique(biomart.anno[biomart.anno$GO %in% c("GO:0006260"), "SYMBOL"])
+    g2m.genes <- unique(biomart.anno[biomart.anno$GO %in% c("GO:0000087", "GO:0000279", "GO:0007059", "GO:0048285"), "SYMBOL"])
+    
+    # Calculate Cell Cycle Scores
+    sob.prs <- CellCycleScoring(sob.prs,
+                               s.features = s.genes,
+                               g2m.features = g2m.genes,
+                               set.ident = TRUE
+    )
+    
+    # Regress Cell Cycle Score
+    sob.prs <- ScaleData(sob.prs,
+                        vars.to.regress = c("S.Score", "G2M.Score"),
+                        features = rownames(sob.prs), verbose = F
+    )
+    
+    # Run PCA
+    sob.prs <- RunPCA(sob.prs,
+                     features = VariableFeatures(sob.prs, selection.method = "dispersion"),
+                     verbose = F, ndims.print = 0, nfeatures.print = 0
+    )
+    
+    # Plot UMAP
+    umap <- DimPlot(sob.p, reduction = "umap", pt.size = 1, group.by = "seurat_clusters") +
+        ggtitle(paste0("Individual: ", individual, " Age: ", age, " Sex: ", sex)) +
+        scale_color_hue(l = 50)
+    
+    # Return
+    return(list(sob = sob.sub, umap = sob.prs))
+    
+})
+#}, mc.cores = detectCores(), mc.set.seed = 123)
 
 # Set Variables to be used later
 i <- "rep1"
