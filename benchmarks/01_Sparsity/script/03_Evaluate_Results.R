a637dc410acd0487eda6876fd96796fb532552f1
spriyansh <spriyansh29@gmail.com>
2023-11-12 17:13:25 +0000

    Zero-inflation Added

--- benchmarks/01_Sparsity/script/03_Evaluate_Results.R
+++ benchmarks/01_Sparsity/script/03_Evaluate_Results.R
@@ -12,13 +12,15 @@ dirPath <- "benchmarks/01_Sparsity/data/output/"
 helpScriptsDir <- "R_Scripts/helper_function/"
 
 # Load helper functions
-source(paste0(helpScriptsDir, "get_performance.R"))
+#source(paste0(helpScriptsDir, "get_performance.R"))
+source(paste0(helpScriptsDir, "get_performance_ROCR_.R"))
+source(paste0(helpScriptsDir, "calculate_metrics_binary.R"))
 
 # Load names of files
 dataSets <- list.files(paste0(dirPath))
 dataSets <- dataSets[!(dataSets %in% c("Accuracy.png", "ROC.png", "Performance.Table.tsv"))]
 names(dataSets) <- str_remove(
-  str_split_i(dataSets, pattern = "\\.", i = 4),
+  str_remove(dataSets, pattern = "scmp.obj.sparsity."),
   ".RData"
 )
 
@@ -35,31 +37,30 @@ for (i in names(dataSets)) {
 
   # Extract the Row Data
   row_data <- as.data.frame(
-    rowData(scmp.obj@sce)
-  )[, c("gene_short_name", "status")]
-
-  # Get the gene-info
-  gene.change <- rownames(row_data[row_data$status != "No_Change", ])
-  gene.no.change <- rownames(row_data[row_data$status == "No_Change", ])
-
-  # Varying R2
-  r2_sequence_value <- seq(0.05, 0.95, 0.1)
-
-  # if (i == "80") {
-  #   r2_sequence_value <- seq(0.05, 0.9, 0.05)
-  # }
+      rowData(scmp.obj@sce))[, c("gene_short_name", "status")]
+  
+  # Set binary labels
+  gene.change <- rep(1, length(rownames(row_data[row_data$status != "No_Change", ])))
+  gene.no.change <-rep(0, length(rownames(row_data[row_data$status == "No_Change", ])))
+  
+  # Add names
+  names(gene.change) <- rownames(row_data[row_data$status != "No_Change", ])
+  names(gene.no.change) <- rownames(row_data[row_data$status == "No_Change", ])
+  
+  # Ground truth
+  groundTruth <- c(gene.change, gene.no.change)
+  
   # Get Performance
-  performance.measure <- get_performance(
-    scmp_obj = scmp.obj,
-    gene_change = gene.change,
-    gene_no_change = gene.no.change,
-    r2_sequence = r2_sequence_value
-  )
-
-  # Add Inflation
-  performance.measure[["Zi"]] <- i
-
+  performance.measure <- as.data.frame(get_performance_ROCR(
+      scmpObj = scmp.obj,
+      groundTruth = groundTruth,
+      r2_sequence = seq(0.00, 0.95, 0.05),
+      include_influ = TRUE
+  ))
+  
   # Add to list
+  performance.measure[["parameter"]] <- "ZI"
+  performance.measure[["parameter.value"]] <- as.numeric(i)
   eval.list[[i]] <- performance.measure
 }
 
