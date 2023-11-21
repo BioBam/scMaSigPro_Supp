05a1500da70b88e069c721d9500ffe7f04617301
spriyansh <spriyansh29@gmail.com>
2023-11-21 22:16:22 +0000

    Added Zero-inflation

--- benchmarks/01_Sparsity/01_Simulate_Data.R
+++ benchmarks/01_Sparsity/01_Simulate_Data.R
@@ -1,4 +1,4 @@
-# Title: Simulate 9 Datasets with different levels of sparsity
+# Title: Simulate 4 Datasets with Different levels of sparsity
 # Author: Priyansh Srivastava
 # Email: spriyansh29@gmail.com
 # Year: 2023
@@ -38,113 +38,131 @@ source(paste0(helpScriptsDir, "plot_simulations().R"))
 source(paste0(helpScriptsDir, "add_gene_anno().R"))
 source(paste0(helpScriptsDir, "calc_bin_size.R"))
 
-#Zero-Inflation
+# Zero-Inflation
 zi <- list(
-    #  "sparsity_50" = -2, # 50 % #
-    #  "sparsity_55" = -1, # 55 % #
-    "sparsity_60" = -0.5, # 60 % #
-    #  "sparsity_65" = -0.2, # 65 % #
-    "sparsity_70" = 0.02,  # 70 % #
-    #  "sparsity_75" = 0.3, # 75 % #
-    "sparsity_80" = 0.7, # 80 %#
-    #  "sparsity_85" = 1.2, # 85 % #
-    "sparsity_90" =2.5 # 90 % #
+  #  "sparsity_50" = -2, # 50 % #
+  #  "sparsity_55" = -1, # 55 % #
+  "sparsity_60" = -0.5, # 60 % #
+  #  "sparsity_65" = -0.2, # 65 % #
+  "sparsity_70" = 0.02, # 70 % #
+  #  "sparsity_75" = 0.3, # 75 % #
+  "sparsity_80" = 0.7, # 80 %#
+  #  "sparsity_85" = 1.2, # 85 % #
+  "sparsity_90" = 2.5 # 90 % #
 )
 
 # Create Base parameters/ Same for All groups
 params.groups <- newSplatParams(
-    batch.rmEffect = TRUE, # No Batch affect
-    batchCells = 3000, # Number of Cells
-    nGenes = 2000, # Number of Genes
-    seed = 2022, # Set seed
-    mean.rate = paramEstimates@mean.rate,
-    mean.shape = paramEstimates@mean.shape,
-    lib.scale = paramEstimates@lib.scale,
-    lib.loc = paramEstimates@lib.loc,
-    bcv.common = paramEstimates@bcv.common,
-    bcv.df = paramEstimates@bcv.df,
-    dropout.type = "experiment",
-    group.prob = c(0.5, 0.5),
-    path.from = c(0, 0),
-    de.prob = 0.3,
-    de.facLoc = 1,
-    out.facLoc = paramEstimates@out.facLoc,
-    dropout.mid = paramEstimates@dropout.mid,
-    out.facScale = paramEstimates@out.facScale,
-    out.prob = paramEstimates@out.prob,
-    path.skew = c(0.5, 0.5),
-    path.nSteps = c(1500, 1500)
+  batch.rmEffect = TRUE, # No Batch affect
+  batchCells = 3000, # Number of Cells
+  nGenes = 2000, # Number of Genes
+  seed = 2022, # Set seed
+  mean.rate = paramEstimates@mean.rate,
+  mean.shape = paramEstimates@mean.shape,
+  lib.scale = paramEstimates@lib.scale,
+  lib.loc = paramEstimates@lib.loc,
+  bcv.common = paramEstimates@bcv.common,
+  bcv.df = paramEstimates@bcv.df,
+  dropout.type = "experiment",
+  group.prob = c(0.5, 0.5),
+  path.from = c(0, 0),
+  de.prob = 0.3,
+  de.facLoc = 1,
+  out.facLoc = paramEstimates@out.facLoc,
+  dropout.mid = paramEstimates@dropout.mid,
+  out.facScale = paramEstimates@out.facScale,
+  out.prob = paramEstimates@out.prob,
+  path.skew = c(0.5, 0.5),
+  path.nSteps = c(1500, 1500)
 )
 
 # Generate Datasets
 parameter.list <- mclapply(names(zi), function(dropout_shape, params_groups = params.groups,
-                                               sce.path = sce_path){
-    
-    # Get Variables
-    total_sparsity <- str_remove(pattern = "sparsity_", dropout_shape)
-    dropout_shape_value <- zi[[dropout_shape]]
-    
-    # Simulate Object
-    sim.sce <- splatSimulate(params = params_groups,
-                             method = "paths",
-                             verbose = F,
-                             dropout.shape = dropout_shape_value
-    )
-    
-    # Sparsity values
-    trueSparsity <- round(sparsity(as.matrix(sim.sce@assays@data@listData$TrueCounts)) * 100)
-    simulatedSparsity <- round(sparsity(as.matrix(sim.sce@assays@data@listData$counts)) * 100) - trueSparsity
-    totSparsity <- round(sparsity(as.matrix(sim.sce@assays@data@listData$counts)) * 100)
-    
-    # Add gene Info
-    gene.info <- add_gene_anno(sim.sce = sim.sce)
-    gene.info <- gene.info[mixedsort(gene.info$gene_short_name), ]
-    
-    # Update the SCE Simulated Object
-    rowData(sim.sce) <- DataFrame(gene.info)
-    
-    # SaveRDS
-    obj.path <- paste0(sce.path, paste0("zi_", totSparsity, ".RData"))
-    save(sim.sce, file = obj.path)
-    
-    # Names
-    label_vector <- c("Total_Sparsity"= totSparsity,
-                      "True_Sparsity" = trueSparsity,
-                      "Simulated_Sparsity" = simulatedSparsity,
-                      "Filename" = paste0("zi_", totSparsity, ".RData")
-    )
-    
-    phateIn <- t(as.matrix(sim.sce@assays@data@listData$counts))
-    keep_cols <- colSums(phateIn > 0) > 10
-    phateIn <- phateIn[,keep_cols]
-    phate_dim <- phate(phateIn, ndim = 2, verbose = F,
-                       knn = 100, 
-                       decay = 100,
-                       t = 50)
-    plt.data <- data.frame(PHATE_1 = phate_dim$embedding[,1],
-                           PHATE_2 = phate_dim$embedding[,2],
-                           Simulated_Steps = sim.sce@colData$Step,
-                           Path = sim.sce@colData$Group)
-    
-    plt <- ggplot(plt.data) +
-        geom_point(aes(x = PHATE_1,
-                       y = PHATE_2,
-                       color = Simulated_Steps,
-                       shape = Path),
-                   size = 1.5) + theme_minimal(base_size = 12) +
-        scale_color_viridis(option="C") + ggtitle(
-            paste("Total ZI:", totSparsity)
-        )
-    
+                                               sce.path = sce_path) {
+  # Get Variables
+  total_sparsity <- str_remove(pattern = "sparsity_", dropout_shape)
+  dropout_shape_value <- zi[[dropout_shape]]
 
-    return(list(parameters = label_vector,
-                plots = plt))
+  # Simulate Object
+  sim.sce <- splatSimulate(
+    params = params_groups,
+    method = "paths",
+    verbose = F,
+    dropout.shape = dropout_shape_value
+  )
+
+  # Sparsity values
+  trueSparsity <- round(sparsity(as.matrix(sim.sce@assays@data@listData$TrueCounts)) * 100)
+  simulatedSparsity <- round(sparsity(as.matrix(sim.sce@assays@data@listData$counts)) * 100) - trueSparsity
+  totSparsity <- round(sparsity(as.matrix(sim.sce@assays@data@listData$counts)) * 100)
+
+  # Add gene Info
+  gene.info <- add_gene_anno(sim.sce = sim.sce)
+  gene.info <- gene.info[mixedsort(gene.info$gene_short_name), ]
+
+  # Update the SCE Simulated Object
+  rowData(sim.sce) <- DataFrame(gene.info)
+
+  # SaveRDS
+  obj.path <- paste0(sce.path, paste0("zi_", totSparsity, ".RData"))
+  save(sim.sce, file = obj.path)
+
+  # Names
+  label_vector <- c(
+    "Total_Sparsity" = totSparsity,
+    "True_Sparsity" = trueSparsity,
+    "Simulated_Sparsity" = simulatedSparsity,
+    "Filename" = paste0("zi_", totSparsity, ".RData")
+  )
+  
+  # Compute PHATE Dimensions
+  phateIn <- t(as.matrix(sim.sce@assays@data@listData$counts))
+  keep_cols <- colSums(phateIn > 0) > 10
+  phateIn <- phateIn[, keep_cols]
+  phate_dim <- phate(phateIn,
+    ndim = 2, verbose = F,
+    knn = 100,
+    decay = 100,
+    t = 50
+  )
+  
+  # Create Plotting frame for PHATE
+  plt.data <- data.frame(
+    PHATE_1 = phate_dim$embedding[, 1],
+    PHATE_2 = phate_dim$embedding[, 2],
+    Simulated_Steps = sim.sce@colData$Step,
+    Path = sim.sce@colData$Group
+  )
+  
+  # Plot PHATE dimensions
+  plt <- ggplot(plt.data) +
+    geom_point(
+      aes(
+        x = PHATE_1,
+        y = PHATE_2,
+        color = Simulated_Steps,
+        shape = Path
+      ),
+      size = 1.5
+    ) +
+    theme_minimal(base_size = 12) +
+    scale_color_viridis(option = "C") +
+    ggtitle(
+      paste("Total Sparsity:", totSparsity)
+    )
+
+  # Return
+  return(list(
+    parameters = label_vector,
+    plots = plt
+  ))
 }, mc.cores = 1)
+# Set names
 names(parameter.list) <- names(zi)
 
 # Extract Parameters
-parameters <- lapply(parameter.list, function(i){
-    return(i[["parameters"]])
+parameters <- lapply(parameter.list, function(i) {
+  return(i[["parameters"]])
 })
 
 # Convert to dataframe
@@ -152,20 +170,14 @@ parameter.frame <- do.call("rbind", parameters)
 
 # Save in text files
 write.table(parameter.frame,
-            file = "Tables/01_ZI_Parameter.Table.tsv",
-            sep = "\t", quote = F, row.names = F)
+  file = "Tables/01_ZI_Parameter.Table.tsv",
+  sep = "\t", quote = F, row.names = F
+)
 
 # Extract Plots
-plots <- lapply(parameter.list, function(i){
-    return(i[["plots"]])
+plots <- lapply(parameter.list, function(i) {
+  return(i[["plots"]])
 })
 
+# Save
 saveRDS(plots, paste0(imgPath, "01_Zi_60_90.RDS"))
-
-# Plot for supplemnetary Material
-combined_pplot <- ggarrange(plots[[1]],plots[[2]], plots[[3]], plots[[4]],
-    ncol = 2, nrow = 2,common.legend = TRUE,legend = "bottom",
-    labels = c("A.","B.","C.","D."))
-
-ggsave(filename = "Figures/SuppData/01_Sim_10_to_90_ZI.png",
-       plot = combined_pplot, dpi = 600, height = 8, width = 14)
--- benchmarks/01_Sparsity/02_Run_ScMaSigPro.R
+++ benchmarks/01_Sparsity/02_Run_ScMaSigPro.R
@@ -10,7 +10,12 @@ suppressPackageStartupMessages(library(scMaSigPro))
 # Set Paths relative to project
 inPath <- "/supp_data/benchmarks/01_Sparsity/simulated/sce/"
 outPath <- "/supp_data/benchmarks/01_Sparsity/output/"
+outPath2 <- "Tables/"
 helpScriptsDir <- "R_Scripts/helper_function/"
+
+# Load helper functions
+source(paste0(helpScriptsDir, "get_performance_ROCR_.R"))
+source(paste0(helpScriptsDir, "calculate_metrics_binary.R"))
 
 # Create Missing Directory
 dir.create(outPath, showWarnings = F, recursive = T)
@@ -22,31 +27,39 @@ names(dataSets) <- str_remove(
   ".RData"
 )
 
-#dataSets <- dataSets[names(dataSets) %in% c("90")]
+# Zero-Inflation.evaluation
+eval.list <- list()
+
+# dataSets <- dataSets[names(dataSets) %in% c("90")]
 
 # Set-up a for loop
 for (i in names(dataSets)) {
-  poly.degree <- 1
+  # Set variables
+  poly.degree <- 2
   drop_fac <- 1
-  maxit<- 100
+  maxit <- 100
   fam <- MASS::negative.binomial(20)
-  split.bins = F
-  
+  split.bins <- F
+
   cat(paste("\nRunning for sparsity:", i))
 
   # Load Data
   load(file = paste0(inPath, dataSets[i]))
-  
+
+  # Evaluate
   tryCatch(
     expr = {
       # Convert
-      scmp.obj <- as_scmp(sim.sce, from = "sce",
-                          align_pseudotime = F,
-                          additional_params = list(
-                              labels_exist = TRUE,
-                              existing_pseudotime_colname = "Step",
-                              existing_path_colname = "Group"), verbose = F)
-      
+      scmp.obj <- as_scmp(sim.sce,
+        from = "sce",
+        align_pseudotime = F,
+        additional_params = list(
+          labels_exist = TRUE,
+          existing_pseudotime_colname = "Step",
+          existing_path_colname = "Group"
+        ), verbose = F
+      )
+
       # Compress
       scmp.obj <- squeeze(
         scmpObject = scmp.obj,
@@ -59,36 +72,73 @@ for (i in names(dataSets)) {
         drop_trails = F,
         fill_gaps = F
       )
-      
+
       # Make Design
       scmp.obj <- sc.make.design.matrix(scmp.obj, poly_degree = poly.degree)
 
       # Run p-vector
       scmp.obj <- sc.p.vector(
-          scmpObj = scmp.obj, verbose = F, min.obs = 1, parallel = F,
-          offset = T, 
-          logOffset = F,
-          useWeights = T, 
-          logWeights = F, 
-          useInverseWeights = T,
-          max_it = maxit, 
-          family = fam)
+        scmpObj = scmp.obj, verbose = F, min.obs = 1, parallel = F,
+        offset = T,
+        logOffset = F,
+        useWeights = T,
+        logWeights = F,
+        useInverseWeights = T,
+        max_it = maxit,
+        family = fam
+      )
 
       # Run-Step-2
       scmp.obj <- sc.T.fit(
-          parallel = T,
-          scmpObj = scmp.obj, verbose = F,
-          step.method = "backward"
-          )
+        parallel = T,
+        scmpObj = scmp.obj, verbose = F,
+        step.method = "backward"
+      )
 
       # Save Object
       save(scmp.obj, file = paste0(outPath, "scmp.obj.zi.", i, ".RData"))
 
       # Validate
       cat(paste("\nCompleted for", i))
+      # Evaluate
+      row_data <- as.data.frame(
+        rowData(scmp.obj@sce)
+      )[, c("gene_short_name", "status")]
+
+      # Set binary labels
+      gene.change <- rep(1, length(rownames(row_data[row_data$status != "No_Change", ])))
+      gene.no.change <- rep(0, length(rownames(row_data[row_data$status == "No_Change", ])))
+
+      # Add names
+      names(gene.change) <- rownames(row_data[row_data$status != "No_Change", ])
+      names(gene.no.change) <- rownames(row_data[row_data$status == "No_Change", ])
+
+      # Ground truth
+      groundTruth <- c(gene.change, gene.no.change)
+
+      # Get Performance
+      performance.measure <- as.data.frame(get_performance_ROCR(
+        scmpObj = scmp.obj,
+        groundTruth = groundTruth,
+        r2_sequence = seq(0.00, 0.95, 0.05),
+        include_influ = TRUE
+      ))
+
+      # Add to list
+      performance.measure[["parameter"]] <- "ZI"
+      performance.measure[["parameter.value"]] <- i
+      eval.list[[i]] <- performance.measure
     },
     error = function(e) {
       cat(paste("\nFailed for", i, "because", e$message))
     }
   )
 }
+
+# Combine
+evaluation.frame <- do.call(rbind, eval.list)
+
+# Write
+write.table(evaluation.frame, paste0(outPath2, "01_ZI_Performance.Table.tsv"),
+  sep = "\t", row.names = F, quote = F
+)
